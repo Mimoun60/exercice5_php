777871b60c695150a7b34c58037fad5f47cc33d5
Mimoun60 <mimoun.boulashoub@bbox.fr>
2016-05-10 10:56:19 +0200

    exo5 php

---
+++ vrai5.php
@@ -0,0 +1,17 @@
+<?php
+
+
+<?php
+
+
+function $prix_ttc ($prix_ht, $taux_tva)
+{
+
+  return ($prix_ht*(1+$taux_tva/100));
+}
+print $prix_ttc (20,21);
+
+$resultat = aire(5,4);
+return $resultat;
+}
+?>
